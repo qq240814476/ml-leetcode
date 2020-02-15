d005018827693fb9716f0b152ccfd2e4fa815948
qq240814476 <240814476@qq.com>
2020-02-15 16:35:10 +0800

    50 pow 二分求幂

---
+++ 50.pow-x-n.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    // if(n === 0) return 1
+    // if(x === 0){
+    //   if(n > 0){
+    //     return 0
+    //   }else{
+    //     return Infinity
+    //   }
+    // }
+    // if(x === 1) return 1
+
+    // if(n < 0){
+    //   x = 1 / x
+    //   n = -n
+    // }
+
+    // let res = 1
+
+    // while(n){
+    //   if(n & 1){
+    //     res *= x
+    //   }
+    //   x *= x
+    //   n = n >> 1
+    // }
+    // return res
+    
+    // return Math.pow(x, n)
+};
+// @lc code=end
+
