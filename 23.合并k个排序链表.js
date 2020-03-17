b5d9a07855c820adcfd079394065f769f1db3983
qq240814476 <240814476@qq.com>
2020-03-18 00:29:23 +0800

    a

---
+++ 23.合并k个排序链表.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=javascript
+ *
+ * [23] 合并K个排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+
+};
+// @lc code=end
+
