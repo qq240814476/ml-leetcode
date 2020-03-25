10d9279a58707378e3a4437aad619b167aedc0ec
qq240814476 <240814476@qq.com>
2020-03-25 23:32:49 +0800

    题目

--- 23.合并k个排序链表.js
+++ 23.合并k个排序链表.js
@@ -17,7 +17,15 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-
+  if(lists.length === 0) return null
+  let head = {
+    val: Number.MIN_VALUE,
+    next: null
+  }
+  for(let i = 0; i<lists.length; i++){
+    if(head)
+  }
+  return head.next
 };
 // @lc code=end
 
