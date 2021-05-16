3d1d6e466e2e21359afcb7d54b08e05d6d47dbc0
malin.996 <malin.996@bytedance.com>
2021-05-16 22:39:44 +0800

    feat: 973. 前K小个数 -- maxHeap

---
+++ Heap/973.最接近原点的-k-个点.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=973 lang=javascript
+ *
+ * [973] 最接近原点的 K 个点
+ */
+
+// @lc code=start
+class Heap{
+  data = [];
+  compare = null;
+  constructor(compare){
+    this.compare = compare;
+  }
+  push(obj){
+      this.data.push(obj);
+      let idx = this.size() - 1;
+      // divided by 除以
+      while(idx && this.compare(this.data[idx], this.data[Math.floor((idx - 1) / 2)])){
+          const temp = this.data[idx];
+          this.data[idx] = this.data[Math.floor((idx - 1) / 2)];
+          this.data[Math.floor((idx - 1) / 2)] = temp;
+          idx = Math.floor((idx - 1) / 2);
+      }
+  }
+  pop(){
+      const top = this.data[0];
+      this.data[0] = this.data[this.size() - 1];
+      this.data.length -= 1;
+      // if(this.data.length == 0) this.data = [];
+      let pIdx = 0,
+          lIdx = 2 * pIdx + 1,
+          rIdx = 2 * pIdx + 2;
+      while(lIdx <= this.size() -1){
+          // 可能是极大也可能是极小
+          let extremumIdx = pIdx;
+          if(this.compare(this.data[lIdx], this.data[extremumIdx])) extremumIdx = lIdx;
+          if(rIdx <= this.size() -1 && this.compare(this.data[rIdx], this.data[extremumIdx]))
+              extremumIdx = rIdx;
+          if(extremumIdx == pIdx) break;
+          const temp = this.data[pIdx];
+          this.data[pIdx] = this.data[extremumIdx];
+          this.data[extremumIdx] = temp;
+          pIdx = extremumIdx;
+          lIdx = 2 * pIdx + 1,
+          rIdx = 2 * pIdx + 2;
+      }
+      return top;
+  }
+  size(){
+      return this.data.length;
+  }
+  top(){
+      return this.data[0];
+  }
+}
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+// c方 = a方 + b方
+// 前 K 个 最小的 小于 最大的, 大顶堆
+var kClosest = function(points, k) {
+    const maxHeap = new Heap((a,b) =>(a.val>b.val))
+    for(const point of points){
+        maxHeap.push({point, val: Math.pow(point[0], 2)+ Math.pow(point[1], 2)})
+        if(maxHeap.size() > k) maxHeap.pop();
+    }
+    return maxHeap.data.map(ele => ele.point);
+};
+// @lc code=end
+
