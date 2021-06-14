188e711700c2caecee3d1e08163091fc619a7bac
malin.996 <malin.996@bytedance.com>
2021-06-14 16:48:46 +0800

    feat: three time

--- sort/merge/23.合并k个排序链表.cpp
+++ sort/merge/23.合并k个排序链表.cpp
@@ -17,24 +17,76 @@
  */
 class Solution {
 public:
+    // struct CMP{
+    //     bool operator()(ListNode * a, ListNode * b){
+    //         return a->val > b->val;
+    //     }
+    // };
+    // ListNode* mergeKLists(vector<ListNode*>& lists) {
+    //     // define a priority queue
+    //     priority_queue<ListNode *, vector<ListNode *>, CMP> q;
+    //     for(int i = 0; i < lists.size(); i++){
+    //         ListNode * p = lists[i];
+    //         while(p){
+    //             q.push(p);
+    //             p = p->next;
+    //         }
+    //     }
+    //     // victural head node
+    //     ListNode h, *p = &h;
+    //     // judge empty
+    //     while(!q.empty()){
+    //         p->next = q.top();
+    //         q.pop();
+    //         p = p->next;
+    //     }
+    //     p->next = nullptr;
+    //     return h.next;
+    // }
+
+    // // 重写仿函数, 小顶堆
+    // struct CMP{
+    //     bool operator()(ListNode * a, ListNode * b){
+    //         return a->val > b->val;
+    //     }
+    // };
+    // ListNode* mergeKLists(vector<ListNode*>& lists) {
+    //     priority_queue<ListNode *, vector<ListNode *>, CMP> q;
+    //     for(auto x: lists){
+    //         ListNode *p = x;
+    //         while(p){
+    //             q.push(p);
+    //             p = p->next;
+    //         }
+    //     }
+    //     // all the listnode in ascending order
+    //     // then we should make  a new linked-list to store them
+    //     ListNode h, *p = &h;
+    //     while(!q.empty()){
+    //         p->next = q.top();
+    //         q.pop();
+    //         p = p->next;
+    //     }
+    //     // tail point nullptr
+    //     p->next = nullptr;
+    //     return h.next;
+    // }
+
     struct CMP{
-        bool operator()(ListNode * a, ListNode * b){
-            return a->val > b->val;
+        bool operator()(ListNode *a, ListNode *b){
+            return a->val > b -> val;
         }
     };
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // define a priority queue
         priority_queue<ListNode *, vector<ListNode *>, CMP> q;
-        for(int i = 0; i < lists.size(); i++){
-            ListNode * p = lists[i];
+        for(auto x: lists){
+            ListNode *p = x;
             while(p){
                 q.push(p);
-                p = p->next;
+                p = p -> next;
             }
         }
-        // victural head node
         ListNode h, *p = &h;
-        // judge empty
         while(!q.empty()){
             p->next = q.top();
             q.pop();
