a1ab3922f1f75eecdd91eb9389ff236a69a6a4a7
qq240814476 <240814476@qq.com>
2021-06-19 11:46:43 +0800

    feat: sec range sum

--- sort/merge-sort/327.区间和的个数.cpp
+++ sort/merge-sort/327.区间和的个数.cpp
@@ -50,46 +50,91 @@
 // @lc code=start
 class Solution {
 public:
-    int res;
-    int low, up;
+    // int res;
+    // int low, up;
+    // int countRangeSum(vector<int>& nums, int lower, int upper) {
+    //     // rangeSum[1,2] = prevSum[2] - prevSum[1-1]
+    //     // so we should get prevSum first
+    //     vector<long long> ps;
+    //     res = 0;
+    //     low = lower;
+    //     up = upper;
+    //     ps.push_back(0);
+    //     for(int i=0;i<nums.size();i++){
+    //         ps.push_back(nums[i] + ps[i]);
+    //     }
+    //     // then we use merge thought , lower <= prevSum[j] - prevSum[i] <= upper
+    //     // use merge_sort
+    //     merge_sort(ps, 0, ps.size()-1);
+    //     return res;
+    // }
+    // void merge_sort(vector<long long> &a, int l, int r){
+    //     if(l >= r) return;
+    //     // split
+    //     int mid = (l+r) >> 1;
+    //     merge_sort(a, l, mid);
+    //     merge_sort(a, mid + 1, r);
+
+    //     // merge
+    //     vector<long long> t;
+    //     int lp = l, rp = mid+1;
+    //     // double point
+    //     int k1 = lp, k2 = lp;
+    //     for(int i = rp; i<= r; i++){
+    //         long long x = a[i] - up;
+    //         long long y = a[i] - low;
+    //         // k1, k2 should include a[k1] <= [x, y] < a[k2]
+    //         while(k1 <= mid && a[k1] < x) k1++;
+    //         while(k2 <= mid && a[k2] <= y) k2++;
+    //         res += k2-k1;
+    //     }
+    //     while(lp <= mid || rp <= r){
+    //         if((rp > r) || (lp <= mid && a[lp] < a[rp])){
+    //             t.push_back(a[lp++]);
+    //         } else {
+    //             t.push_back(a[rp++]);
+    //         }
+    //     }
+    //     for(int i = l; i<=r; i++) a[i] = t[i - l];
+    //     return ;
+    // }
+    int res, low, up;
     int countRangeSum(vector<int>& nums, int lower, int upper) {
-        // rangeSum[1,2] = prevSum[2] - prevSum[1-1]
-        // so we should get prevSum first
+        // calculate prevSum
         vector<long long> ps;
-        res = 0;
-        low = lower;
-        up = upper;
         ps.push_back(0);
         for(int i=0;i<nums.size();i++){
             ps.push_back(nums[i] + ps[i]);
         }
-        // then we use merge thought , lower <= prevSum[j] - prevSum[i] <= upper
-        // use merge_sort
-        merge_sort(ps, 0, ps.size()-1);
+        res = 0;
+        low = lower;
+        up = upper;
+        merge_sort(ps, 0, nums.size());
         return res;
     }
+    // &a
     void merge_sort(vector<long long> &a, int l, int r){
-        if(l >= r) return;
+        if(l >= r) return ;
         // split
         int mid = (l+r) >> 1;
         merge_sort(a, l, mid);
         merge_sort(a, mid + 1, r);
 
-        // merge
-        vector<long long> t;
-        int lp = l, rp = mid+1;
-        // double point
-        int k1 = lp, k2 = lp;
-        for(int i = rp; i<= r; i++){
-            long long x = a[i] - up;
-            long long y = a[i] - low;
-            // k1, k2 should include a[k1] <= [x, y] < a[k2]
-            while(k1 <= mid && a[k1] < x) k1++;
-            while(k2 <= mid && a[k2] <= y) k2++;
-            res += k2-k1;
+        // plus plus res
+        int k1 = l, k2 = l;
+        for(int i = mid+1;i<=r; i++){
+            int x = a[i] - up;
+            int y = a[i] - low;
+            while(k1 <=mid && a[k1] < x) k1++;
+            while(k2 < mid && a[k2] <= y) k2++;
+            res += k2 - k1;
         }
+
+        //merge 
+        vector<long long> t;
+        int lp = l, rp = mid + 1;
         while(lp <= mid || rp <= r){
-            if((rp > r) || (lp <= mid && a[lp] < a[rp])){
+            if((rp == r) || (lp <= mid && a[lp] < a[rp])){
                 t.push_back(a[lp++]);
             } else {
                 t.push_back(a[rp++]);
